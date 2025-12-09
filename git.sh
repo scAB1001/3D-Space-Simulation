fbf8e58f6e35040ec7947dbb4a242cf9341dfd52
scAB1001 <sc222ab@leeds.ac.uk>
2025-12-09 17:39:40 +0000

    feat: Cleaning git bash script

--- git.sh
+++ git.sh
@@ -3,11 +3,9 @@
 # Run chmod +x git.sh once to make this executable
 # Create symbolic link `ln -s git.sh git` to run as just ./git
 
-git pull origin andreas; git st
-# cd ~/github-projects/uni/graphics/
+git pull origin dev; git st
 git add .; git commit
-# git push origin andreas
-git push origin archie
+git push origin dev
 cd ./cw2/
 
 # Set vim.basic as default with:
