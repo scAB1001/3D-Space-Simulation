bed66b486ca170000e7360b22c520a95335cc7f7
Andreas Bazigos <sc222ab@leeds.ac.uk>
2025-12-12 02:56:22 +0000

    feat: Submitting

--- cw2/main/texture.cpp
+++ cw2/main/texture.cpp
@@ -9,8 +9,8 @@
 GLuint load_texture_2d( char const* aPath )
 {
 	/* References for code inspiration:
-	 * - https: // learnopengl.com/Getting-started/Textures
-	 */
+	* - https: // learnopengl.com/Getting-started/Textures
+	*/
 
 	assert( aPath );
 
